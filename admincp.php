f1d91e277918dccaf42ea19e4d21bdaf81161431
Mitchell M <mitchell.murphy96@gmail.com>
2017-04-05 15:55:49 -0400

    Beginnings of administration panel

---
+++ admincp.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <style type="text/css">
+            h2 {
+                text-align: center;
+            }
+            
+            #navbar ul {
+                margin: 0;
+                padding: 0;
+                list-style-type: none;
+                text-align: center;
+            }
+            
+            #navbar ul li {
+                display: inline;
+                font-size: 32px;
+                padding-right: 50px;
+                padding-left: 50px;
+            }
+        </style>
+        <h2>Admin Panel</h2>
+        <div id="navbar">
+            <ul>
+                <li>OP1</li>
+                <li>OP2</li>
+                <li>OP3</li>
+                <li>OP4</li>
+            </ul>
+        </div>
+    </head>
+    <body>
+        
+    </body>
+    <footer>
+        
+    </footer>
+</html>
