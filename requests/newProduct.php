fce96c83dbafe41ef025e7544c7d09887e6290c1
Mitchell M <mitchell.murphy96@gmail.com>
2017-04-07 23:00:54 -0400

    Product insertion form completed

--- requests/newProduct.php
+++ requests/newProduct.php
@@ -7,6 +7,43 @@
         margin-top: 45px;
     }
 </style>
+<?php
+if(!isset($_POST['submit'])) {
+    $query = new QueryBuilder();
+    $query->select(array('CategoryName', 'CategoryID'))->from('Categories');
+    $categories = $query->get();
+    $n = count($categories);
+    ?>
+<table>
+    <tr>
+        <th colspan="2">
+            VALID CATEGORY CODES
+        </th>
+    </tr>
+    <tr>
+        <th>
+            CATEGORY
+        </th>
+        <th>
+            CATEGORY ID
+        </th>
+    </tr>
+    <?php
+    for($i = 0; $i < $n; $i++) {
+        ?>
+    <tr>
+        <td>
+            <?php echo $categories[$i]['CategoryName']; ?>
+        </td>
+        <td>
+            <?php echo $categories[$i]['CategoryID']; ?>
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
+<form action="?request=newProduct" method="post">
 <table>
     <tr>
         <td>
@@ -53,15 +90,7 @@
             DiscountPerccent
         </td>
         <td>
-            <input type="text" name="DiscountPerccent" />
-        </td>
-    </tr>
-    <tr>
-        <td>
-            DateAdded
-        </td>
-        <td>
-            <input type="text" name="DateAdded" />
+            <input type="text" name="DiscountPercent" />
         </td>
     </tr>
     <tr>
@@ -78,3 +107,15 @@
         </td>
     </tr>
 </table>
+</form>
+<?php
+} else {
+    $qry = new QueryBuilder();
+    $qry->insert_into('Products', array('CategoryID' => $_POST['CategoryID'], 'ProductCode' => $_POST['ProductCode'], 
+            'ProductName' => $_POST['ProductName'], 'Description' => $_POST['Description'], 'ListPrice' => $_POST['ListPrice'], 
+            'DiscountPercent' => $_POST['DiscountPercent'], 'image' => $_POST['image']));
+    $qry->exec();
+    echo $qry->retrieve();
+    echo "Product inserted.";
+}
+?>
